3e92b4fa3c3a95aefd692d4d7b7ad18308233d02
Jan Duldhardt <duldhardtjan@gmail.com>
2019-02-17 20:29:43 +0700

    Working Ticket

---
+++ app/src/main/java/com/example/jan/eventmanagment/TodayPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jan.eventmanagment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TodayPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_today_page)
+    }
+}
